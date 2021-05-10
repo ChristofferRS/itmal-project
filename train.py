06a7217610690f94c8ef649f7c0de8353d005df0
chri87d9 <christoffer143@gmail.com.com>
2021-05-10 16:37:11 +0200

    acc

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ if __name__=="__main__":
     model=build_model(train)
     print(model.summary())
 
-    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),metrics=['accuracy'])
     EPOCHS = 20
     stopper=tf.keras.callbacks.EarlyStopping(patience=5)
     history = model.fit(train, validation_data=val_ds, epochs=EPOCHS, callbacks=stopper)
